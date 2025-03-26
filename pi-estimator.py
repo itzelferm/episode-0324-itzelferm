abb6a59427be352140e8845edbba0c866ca0be23
Itzel <itzelfrmna@gmail.com>
2025-03-25 23:16:10 -0700

    Finalized Code

--- pi-estimator.py
+++ pi-estimator.py
@@ -12,8 +12,10 @@ total_inside_darts = 0
 for i in range(total_darts):
     x = random.random()
     y = random.random()
-    print(x,y)
 
     if x * x + y * y <= 1:
         total_inside_darts += 1
 print(f"Inside darts : {total_inside_darts}, total darts: {total_darts}")
+
+pi_estimator = 4 * total_inside_darts/total_darts
+print(f"The π estimate is: {pi_estimator}")
